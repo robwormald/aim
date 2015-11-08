0cd6375bf2ddef883219322ae3408b17b0005679
Chris Price <price.c@gmail.com>
2015-11-08 20:19:41 +0000

    Frame-align render calls to prevent flood of updates when window is re-focused

--- src/components/graph/graph.ts
+++ src/components/graph/graph.ts
@@ -1,4 +1,5 @@
 var d3 = require('d3');
+var fc = require('d3fc');
 import chart from './chart';
 import {Component, View, Input, ElementRef} from 'angular2/angular2';
 import {Ticker} from '../../services/TickerService';
@@ -17,28 +18,32 @@ export class StockGraph {
   values: any[] = [];
   chart: any = chart();
 
+  render: () => void;
+
   constructor(private el:ElementRef) {
-
-  }
-
-  render(latestValue){
-    // de-serialise the date into a real Date object
-    latestValue.date = new Date(latestValue.date);
-
-    // limit to latest 40 values
-    this.values.push(latestValue);
-    if (this.values.length > 40) {
-      this.values.shift();
-    }
-
-    // associate data with selection and render chart
-    d3.select(this.el.nativeElement)
-      .datum(this.values)
-      .call(this.chart);
+    // de-couple rendering from incomming messages to prevent
+    // a flood of updates (e.g. when the window regains focus)
+    this.render = fc.util.render(() => {
+      // associate data with selection and render chart
+      d3.select(this.el.nativeElement)
+        .datum(this.values)
+        .call(this.chart);
+    });
   }
 
   onInit() {
-    this.subscription = this.ticker.ticks.subscribe(tick => this.render(tick));
+    this.subscription = this.ticker.ticks.subscribe((latestValue: any) => {
+      // de-serialise the date into a real Date object
+      latestValue.date = new Date(latestValue.date);
+
+      // limit to latest 40 values
+      this.values.push(latestValue);
+      if (this.values.length > 40) {
+        this.values.shift();
+      }
+
+      this.render();
+    });
   }
 
   onDestroy() {
